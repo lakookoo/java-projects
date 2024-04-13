f16ca04f42555a7b7d45f3dd1ebe5a6e9c17b4de
Larisa <kosareva.larisa@gmail.com>
2024-04-13 21:49:43 +0200

    tic-tac-toe game

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -2,17 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         Scanner scanner = new Scanner(System.in);
+        int input;
+        char turn = 'X';
+        int currentTurn = 1;
+        while (true) {
+            do {
+                System.out.println("Enter a position");
+                input = scanner.nextInt();
+                pos[input - 1] = turn;
+            } while (pos[input - 1] == 'X' || pos[input - 1] == 'O');
 
-        System.out.println("What is your age ?");
-        String age = scanner.next();
-        int olderAge = Integer.parseInt(age)*2;
-        System.out.format("You lived %s years. In another %s years you will be %d years old ", age, age, olderAge);
+            System.out.println("  " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+            System.out.println("___________");
+            System.out.println("  " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
+            System.out.println("___________");
+            System.out.println("  " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
 
-        System.out.println("What is your name?");
-        int lengthOfName = scanner.next().length();
-        System.out.format("Your name has %d letters ", lengthOfName);
+            if (pos[0] == turn && pos[1] == turn && pos[2] == turn
+                    || pos[3] == turn && pos[4] == turn && pos[5] == turn
+                    || pos[6] == turn && pos[7] == turn && pos[8] == turn
+                    || pos[6] == turn && pos[3] == turn && pos[0] == turn
+                    || pos[7] == turn && pos[4] == turn && pos[1] == turn
+                    || pos[8] == turn && pos[5] == turn && pos[2] == turn
+                    || pos[6] == turn && pos[4] == turn && pos[2] == turn
+                    || pos[8] == turn && pos[4] == turn && pos[0] == turn){
+                System.out.println(turn + "is the winner");
+                break;
+            }
 
+            if (turn == 'X') {
+                turn = 'O';
+            } else if (turn == 'O'){
+                turn = 'X';
+            }
+            currentTurn++;
+            if(currentTurn > 9){
+                System.out.println("DRAW");
+                break;
+            }
+        }
 
     }
 }
