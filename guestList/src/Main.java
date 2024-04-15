a08090446f434332c1c0592a6b6b240d8ebc5492
Larisa <kosareva.larisa@gmail.com>
2024-04-15 11:07:46 +0200

    rearranging an array of guests after deleteng a guest

--- guestList/src/Main.java
+++ guestList/src/Main.java
@@ -1,29 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    static String[] guests = new String[10];
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        String[] guests = new String[10];
-        Scanner scanner = new Scanner(System.in);
+
+
 
         do {
             System.out.println();
-
+            displayGuests();
             System.out.println();
-            System.out.println("______________________________________\n -Guests-\n");
-            for (String guest : guests) {
-                System.out.println(guest == null ? "--" : guest);
-            }
-            System.out.println("What do you want to do next? ");
-            System.out.println("______________________________________\n -Menu-\n");
-            System.out.println("1 - Add guest");
-            System.out.println("2 - Remove guest");
-            System.out.println("3 - Exit");
-            System.out.println();
+            displayMenu();
+            int option = getOption();
 
-            System.out.println("Option: ");
-
-            int option = scanner.nextInt();
 
            if (option == 3) {
                 break;
@@ -40,15 +31,47 @@ public class Main {
             } else if (option == 2) {
                 System.out.println("Name...");
                 String name = scanner.next();
+                String[] temp = new String[guests.length];
+                int ti = 0;
                 for (int i = 0; i < guests.length; i++) {
                     if (guests[i] != null && guests[i].equals(name)) {
                         guests[i] = null;
                         break;
                     }
                 }
+               for (int i = 0; i < temp.length; i++) {
+                   if (guests[i] != null) {
+                       temp[ti] = guests[i];
+                       ti++;
+                   }
+               }
             }
         } while (true);
         System.out.println("Exiting ...");
 
+    }
+
+    static void displayGuests(){
+        System.out.println("______________________________________\n -Guests-\n");
+        for (int i = 0; i < guests.length; i++) {
+            System.out.println((i+1)+ ". " + (guests[i] == null ?  ". --" : guests[i] ));
+        }
+    }
+
+    static void displayMenu(){
+        System.out.println("What do you want to do next? ");
+        System.out.println("______________________________________\n -Menu-\n");
+        System.out.println("1 - Add guest");
+        System.out.println("2 - Remove guest");
+        System.out.println("3 - Exit");
+        System.out.println();
+
+
+    }
+
+    static int getOption(){
+        System.out.println("Option: ");
+        int option = scanner.nextInt();
+        return option;
     }
 }
