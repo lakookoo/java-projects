383ee972a3becb157867eb226820994190ef1698
Larisa <kosareva.larisa@gmail.com>
2024-04-15 12:31:27 +0200

    insert guest at a given number

--- guestList/src/Main.java
+++ guestList/src/Main.java
@@ -16,12 +16,15 @@ public class Main {
             int option = getOption();
 
 
-            if (option == 3) {
+            if (option == 5) {
                 break;
             } else if (option == 1) {
                 addGuest();
-
             } else if (option == 2) {
+                insertGuest();
+            } else if (option == 3) {
+                renameGuest();
+            } else if (option == 4) {
                 removeGuest();
             }
         } while (true);
@@ -38,7 +41,7 @@ public class Main {
                 isEmpty = false;
             }
         }
-        if (isEmpty){
+        if (isEmpty) {
             System.out.println("guests list is empty");
         }
     }
@@ -47,8 +50,10 @@ public class Main {
         System.out.println("What do you want to do next? ");
         System.out.println("______________________________________\n -Menu-\n");
         System.out.println("1 - Add guest");
-        System.out.println("2 - Remove guest");
-        System.out.println("3 - Exit");
+        System.out.println("2 - Insert guest");
+        System.out.println("3 - Rename guest");
+        System.out.println("4 - Remove guest");
+        System.out.println("5 - Exit");
         System.out.println();
 
 
@@ -57,6 +62,7 @@ public class Main {
     static int getOption() {
         System.out.println("Option: ");
         int option = scanner.nextInt();
+        scanner.nextLine();
         return option;
     }
 
@@ -64,7 +70,7 @@ public class Main {
         for (int i = 0; i < guests.length; i++) {
             if (guests[i] == null) {
                 System.out.println("Name...");
-                String name = scanner.next();
+                String name = scanner.nextLine();
                 guests[i] = name;
                 break;
             }
@@ -72,16 +78,16 @@ public class Main {
     }
 
     static void removeGuest() {
-        System.out.println("Name...");
-        String name = scanner.next();
+        System.out.println("Number...");
+        int number = scanner.nextInt();
+
+        if (guests[number] == null) {
+            System.out.println("There is no guest with number" + number);
+        } else {
+            guests[number - 1] = null;
+        }
         String[] temp = new String[guests.length];
         int ti = 0;
-        for (int i = 0; i < guests.length; i++) {
-            if (guests[i] != null && guests[i].equals(name)) {
-                guests[i] = null;
-                break;
-            }
-        }
         for (int i = 0; i < temp.length; i++) {
             if (guests[i] != null) {
                 temp[ti] = guests[i];
@@ -90,11 +96,43 @@ public class Main {
         }
     }
 
-    static void insertTestNames(){
+    static void insertTestNames() {
         guests[0] = "Jacob";
         guests[1] = "July";
         guests[2] = "Jucy";
         guests[3] = "Jungly";
         guests[4] = "Junior";
+    }
+
+    static void renameGuest() {
+        System.out.println("Number... ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if (num >= 1 && num <= 10 && guests[num - 1] != null) {
+            System.out.println("Name...");
+            String name = scanner.nextLine();
+            guests[num - 1] = name;
+        } else {
+            System.out.println("THere is no guest with that number");
+        }
+    }
+
+    static void insertGuest() {
+        System.out.println("Number... ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if (num >= 1 && num <= 10 && guests[num - 1] != null) {
+            System.out.println("Name...");
+            String name = scanner.nextLine();
+
+            for (int i = guests.length - 1; i > num - 1; i--) {
+                guests[i] = guests[i-1];
+            }
+            guests[num - 1] = name;
+        } else {
+            System.out.println("THere is no guest with that number");
+        }
     }
 }
