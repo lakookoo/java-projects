a7a3371e9ca00d56498275fb11d47c521733edf4
Larisa <kosareva.larisa@gmail.com>
2024-04-17 14:13:42 +0200

    interfaces

---
+++ interfaces/src/Foo.java
@@ -0,0 +1,17 @@
+public class Foo implements Baz{
+
+    @Override
+    public boolean m2() {
+        return false;
+    }
+
+    @Override
+    public String m3(int pos, String tag) {
+        return "";
+    }
+
+    @Override
+    public void m1() {
+
+    }
+}
